c0b554c40f33f8406e2b8cd67f404d20712d2b91
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-11-07 20:49:15 +0200

    put edge in its own implementation file (avoids recompiling everything)

---
+++ edge.cpp
@@ -0,0 +1,225 @@
+/* 
+ *  Copyright 2011 Anders Wallin (anders.e.e.wallin "at" gmail.com)
+ *  
+ *  This file is part of OpenVoronoi.
+ *
+ *  OpenVoronoi is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenVoronoi is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenVoronoi.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "edge.hpp"
+
+namespace ovd {
+
+EdgeProps::EdgeProps() {
+    x[0] = 0;
+    x[1] = 0;
+    x[2] = 0;
+    x[3] = 0;
+    x[4] = 0;
+    x[5] = 0;
+    x[6] = 0;        
+    x[7] = 0;
+    y[0] = 0;
+    y[1] = 0;
+    y[2] = 0;
+    y[3] = 0;
+    y[4] = 0;
+    y[5] = 0;
+    y[6] = 0;        
+    y[7] = 0;
+}
+
+/*
+* bisector formulas
+* x = x1 - x2 - x3*t +/- x4 * sqrt( square(x5+x6*t) - square(x7+x8*t) )
+* (same formula for y-coordinate)
+* line (line/line)
+* parabola (circle/line)
+* hyperbola (circle/circle)
+* ellipse (circle/circle)
+*/
+Point EdgeProps::point(double t) const {
+	double discr1 = round( sq(x[4]+x[5]*t) - sq(x[6]+x[7]*t) );
+	double discr2 = round( sq(y[4]+y[5]*t) - sq(y[6]+y[7]*t) );
+	if ( (discr1 >= 0) && (discr2 >= 0) ) {
+	    double psig = sign ? +1 : -1;
+	    double nsig = sign ? -1 : +1;
+	    double xc = x[0] - x[1] - x[2]*t + psig * x[3] * sqrt( discr1 );
+	    double yc = y[0] - y[1] - y[2]*t + nsig * y[3] * sqrt( discr2 );
+	    if (xc!=xc) { // test for NaN!
+		std::cout << xc << " , " << yc << " t=" << t << "\n";
+		print_params();
+		assert(0);
+	    }
+	    return Point(xc,yc);
+	} else {
+	    std::cout << " warning bisector sqrt(-1) discr1=" << discr1 << " discr2=" << discr2 << "!\n";
+	    std::cout << " t= " << t << "\n";
+	    assert(0);
+	    return Point(0,0);
+	}
+}
+
+void EdgeProps::set_parameters(Site* s1, Site* s2, bool sig) {
+    sign = sig;
+    if (s1->isPoint() && s2->isPoint())        // PP
+        set_pp_parameters(s1,s2);
+    else if (s1->isPoint() && s2->isLine())    // PL
+        set_pl_parameters(s1,s2);
+    else if (s2->isPoint() && s1->isLine())  {  // LP
+        set_pl_parameters(s2,s1);
+        sign = !sign;
+    } else if (s1->isLine() && s2->isLine())     // LL
+        set_ll_parameters(s2,s1);
+    else
+        assert(0);
+        // AP
+        // PA
+        // AA
+        // AL
+        // LA
+}
+
+void EdgeProps::copy_parameters(EdgeProps& other) {
+    sign = other.sign;
+    x[0] = other.x[0];
+    x[1] = other.x[1];
+    x[2] = other.x[2];
+    x[3] = other.x[3];
+    x[4] = other.x[4];
+    x[5] = other.x[5];
+    x[6] = other.x[6];        
+    x[7] = other.x[7];
+    y[0] = other.y[0];
+    y[1] = other.y[1];
+    y[2] = other.y[2];
+    y[3] = other.y[3];
+    y[4] = other.y[4];
+    y[5] = other.y[5];
+    y[6] = other.y[6];        
+    y[7] = other.y[7];
+}   
+
+// called for point(s1)-point(s2) edges
+void EdgeProps::set_pp_parameters(Site* s1, Site* s2) {
+    //std::cout << "set_pp_parameters()\n";
+    // x = x1 - x2 - x3*t +/- x4 * sqrt( square(x5+x6*t) - square(x7+x8*t) )
+    assert( s1->isPoint() && s2->isPoint() );
+    double d = (s1->position() - s2->position()).norm(); //sqrt( sq(xc1-xc2) + sq(yc1-yc2) )
+    double alfa1 = (s2->x() - s1->x()) / d;
+    double alfa2 = (s2->y() - s1->y()) / d;
+    double alfa3 = -d/2;
+    
+    type = LINE;
+    x[0]=s1->x();       
+    x[1]=alfa1*alfa3; // 
+    x[2]=0;  
+    x[3]=-alfa2;       
+    x[4]=0;             
+    x[5]=+1;          
+    x[6]=alfa3;       
+    x[7]=0;           // sqrt(  (0+t)^2 - (d/2 +0*t)^2 )
+    y[0]=s1->y();     
+    y[1]=alfa2*alfa3; 
+    y[2]=0; 
+    y[3]=-alfa1;       
+    y[4]=0;           
+    y[5]=+1;          
+    y[6]=alfa3;       
+    y[7]=0;
+}
+
+// called for point(s1)-line(s2) edges
+void EdgeProps::set_pl_parameters(Site* s1, Site* s2) {
+    assert( s1->isPoint() && s2->isLine() );
+    
+    type = PARABOLA;
+    double alfa3 = s2->a()*s1->x() + s2->b()*s1->y() + s2->c();
+    // figure out k, i.e. offset-direction for LineSite
+    double kk = 1.0;
+    if (alfa3>0.0) {
+        kk = -1.0;
+    } else {
+        sign = !sign;
+    }
+    
+    x[0]=s1->x();       // xc1
+    x[1]=s2->a()*alfa3; // alfa1*alfa3
+    x[2]=s2->a()*kk;      // -alfa1 = - a2 * k2?
+    x[3]=s2->b();       // alfa2 = b2
+    x[4]=0;             // alfa4 = r1 
+    x[5]=+1;            // lambda1 (allways positive offset from PointSite?)
+    x[6]=alfa3;        // alfa3= a2*xc1+b2*yc1+d2?
+    x[7]=kk;            // -1 = k2 side of line??
+
+    y[0]=s1->y();       // yc1
+    y[1]=s2->b()*alfa3; // alfa2*alfa3
+    y[2]=s2->b()*kk;      // -alfa2 = -b2
+    y[3]=s2->a();       // alfa1 = a2
+    y[4]=0;             // alfa4 = r1
+    y[5]=+1;            // lambda1 (allways positive offset from PointSite?)
+    y[6]=alfa3;         // alfa3
+    y[7]=kk;            // -1 = k2 side of line??
+}
+
+// line(s1)-line(s2) edge
+void EdgeProps::set_ll_parameters(Site* s1, Site* s2) {  // Held thesis p96
+    assert( s1->isLine() && s2->isLine() );
+    std::cout << " set_ll \n";
+    type = LINELINE;
+    //sign = true; //sign does not matter because there is no sqrt()?        
+    
+
+    double delta =  s1->a()*s2->b() - s1->b()*s2->a() ;
+    assert( delta != 0 );
+
+    //double kk=-1.0;
+    //if (delta>0)
+    //    kk=+1.0;
+
+    x[0]= ( (s1->b() * s2->c()) - (s2->b() * s1->c()) ) / delta;  // alfa1 = (b1*d2-b2*d1) / delta
+    x[2]= -(s1->k()*s2->b()-s2->k()*s1->b())/delta; // -alfa3 = -( b2-b1 )
+    //x[2]= -(s2->b()-s1->b())/delta; // -alfa3 = -( b2-b1 )
+
+    y[0]= ( (s2->a()*s1->c()) - (s1->a()*s2->c()) ) / delta;  // alfa2 = (a2*d1-a1*d2) / delta        
+    y[2]= -(s2->k()*s1->a()-s1->k()*s2->a())/delta;  // -alfa4 = -( a1-a2 )
+    //y[2]= -(s1->a()-s2->a())/delta;  // -alfa4 = -( a1-a2 )
+
+    x[1]=0;
+    x[3]=0;  
+    x[4]=0;
+    x[5]=0;
+    x[6]=0;
+    x[7]=0;
+    y[1]= 0;
+    y[3]=0; 
+    y[4]=0;
+    y[5]=0;
+    y[6]=0;
+    y[7]=0;
+}
+
+void EdgeProps::print_params() const {
+    std::cout << "x-params: ";
+    for (int m=0;m<8;m++)
+        std::cout << x[m] << " ";
+    std::cout << "\n";
+    std::cout << "y-params: ";
+    for (int m=0;m<8;m++)
+        std::cout << y[m] << " ";
+    std::cout << "\n";
+}
+
+} // end namespace
--- edge.hpp
+++ edge.hpp
@@ -19,7 +19,13 @@
 
 #pragma once
 
+#include <cassert>
+#include <cmath>
+
+#include <boost/graph/adjacency_list.hpp>
+
 #include "point.hpp"
+#include "site.hpp"
 
 namespace ovd {
 
@@ -43,24 +49,7 @@ enum VoronoiEdgeType {LINE, LINELINE, OUTEDGE, PARABOLA, ELLIPSE, HYPERBOLA, SEP
 /// each edge stores a pointer to the next HEEdge 
 /// and the HEFace to which this HEEdge belongs
 struct EdgeProps {
-    EdgeProps() {
-        x[0] = 0;
-        x[1] = 0;
-        x[2] = 0;
-        x[3] = 0;
-        x[4] = 0;
-        x[5] = 0;
-        x[6] = 0;        
-        x[7] = 0;
-        y[0] = 0;
-        y[1] = 0;
-        y[2] = 0;
-        y[3] = 0;
-        y[4] = 0;
-        y[5] = 0;
-        y[6] = 0;        
-        y[7] = 0;
-    }
+    EdgeProps();
     EdgeProps(HEEdge n, HEFace f): next(n), face(f) {}
     /// create edge with given next, twin, and face
     EdgeProps(HEEdge n, HEEdge t, HEFace f): next(n), twin(t), face(f) {}
@@ -85,36 +74,9 @@ struct EdgeProps {
             return x;
     }
     
-    /*
-     * bisector formulas
-     * x = x1 - x2 - x3*t +/- x4 * sqrt( square(x5+x6*t) - square(x7+x8*t) )
-     * (same formula for y-coordinate)
-     * line (line/line)
-     * parabola (circle/line)
-     * hyperbola (circle/circle)
-     * ellipse (circle/circle)
-    */
-    Point point(double t) const {
-        double discr1 = round( sq(x[4]+x[5]*t) - sq(x[6]+x[7]*t) );
-        double discr2 = round( sq(y[4]+y[5]*t) - sq(y[6]+y[7]*t) );
-        if ( (discr1 >= 0) && (discr2 >= 0) ) {
-            double psig = sign ? +1 : -1;
-            double nsig = sign ? -1 : +1;
-            double xc = x[0] - x[1] - x[2]*t + psig * x[3] * sqrt( discr1 );
-            double yc = y[0] - y[1] - y[2]*t + nsig * y[3] * sqrt( discr2 );
-            if (xc!=xc) { // test for NaN!
-                std::cout << xc << " , " << yc << " t=" << t << "\n";
-                print_params();
-                assert(0);
-            }
-            return Point(xc,yc);
-        } else {
-            std::cout << " warning bisector sqrt(-1) discr1=" << discr1 << " discr2=" << discr2 << "!\n";
-            std::cout << " t= " << t << "\n";
-            assert(0);
-            return Point(0,0);
-        }
-    }
+
+    Point point(double t) const; 
+
 
     double minimum_t( Site* s1, Site* s2) {
         if (s1->isPoint() && s2->isPoint())        // PP
@@ -141,152 +103,13 @@ struct EdgeProps {
         assert( mint >=0 );
         return mint;
     }
-    void copy_parameters(EdgeProps& other) {
-        sign = other.sign;
-        x[0] = other.x[0];
-        x[1] = other.x[1];
-        x[2] = other.x[2];
-        x[3] = other.x[3];
-        x[4] = other.x[4];
-        x[5] = other.x[5];
-        x[6] = other.x[6];        
-        x[7] = other.x[7];
-        y[0] = other.y[0];
-        y[1] = other.y[1];
-        y[2] = other.y[2];
-        y[3] = other.y[3];
-        y[4] = other.y[4];
-        y[5] = other.y[5];
-        y[6] = other.y[6];        
-        y[7] = other.y[7];
-    }   
-    // sign is the branch of the quadratic!
-    void set_parameters(Site* s1, Site* s2, bool sig) {
-        sign = sig;
-        if (s1->isPoint() && s2->isPoint())        // PP
-            set_pp_parameters(s1,s2);
-        else if (s1->isPoint() && s2->isLine())    // PL
-            set_pl_parameters(s1,s2);
-        else if (s2->isPoint() && s1->isLine())  {  // LP
-            set_pl_parameters(s2,s1);
-            sign = !sign;
-        } else if (s1->isLine() && s2->isLine())     // LL
-            set_ll_parameters(s2,s1);
-        else
-            assert(0);
-            // AP
-            // PA
-            // AA
-            // AL
-            // LA
-    }
-    // called for point(s1)-point(s2) edges
-    void set_pp_parameters(Site* s1, Site* s2) {
-        //std::cout << "set_pp_parameters()\n";
-        // x = x1 - x2 - x3*t +/- x4 * sqrt( square(x5+x6*t) - square(x7+x8*t) )
-        assert( s1->isPoint() && s2->isPoint() );
-        double d = (s1->position() - s2->position()).norm(); //sqrt( sq(xc1-xc2) + sq(yc1-yc2) )
-        double alfa1 = (s2->x() - s1->x()) / d;
-        double alfa2 = (s2->y() - s1->y()) / d;
-        double alfa3 = -d/2;
-        
-        type = LINE;
-        x[0]=s1->x();       
-        x[1]=alfa1*alfa3; // 
-        x[2]=0;  
-        x[3]=-alfa2;       
-        x[4]=0;             
-        x[5]=+1;          
-        x[6]=alfa3;       
-        x[7]=0;           // sqrt(  (0+t)^2 - (d/2 +0*t)^2 )
-        y[0]=s1->y();     
-        y[1]=alfa2*alfa3; 
-        y[2]=0; 
-        y[3]=-alfa1;       
-        y[4]=0;           
-        y[5]=+1;          
-        y[6]=alfa3;       
-        y[7]=0;
-    }
-    // called for point(s1)-line(s2) edges
-    void set_pl_parameters(Site* s1, Site* s2) {
-        assert( s1->isPoint() && s2->isLine() );
-        
-        type = PARABOLA;
-        double alfa3 = s2->a()*s1->x() + s2->b()*s1->y() + s2->c();
-        // figure out k, i.e. offset-direction for LineSite
-        double kk = 1.0;
-        if (alfa3>0.0) {
-            kk = -1.0;
-        } else {
-            sign = !sign;
-        }
-        
-        x[0]=s1->x();       // xc1
-        x[1]=s2->a()*alfa3; // alfa1*alfa3
-        x[2]=s2->a()*kk;      // -alfa1 = - a2 * k2?
-        x[3]=s2->b();       // alfa2 = b2
-        x[4]=0;             // alfa4 = r1 
-        x[5]=+1;            // lambda1 (allways positive offset from PointSite?)
-        x[6]=alfa3;        // alfa3= a2*xc1+b2*yc1+d2?
-        x[7]=kk;            // -1 = k2 side of line??
+    void copy_parameters(EdgeProps& other);   
+    void set_parameters(Site* s1, Site* s2, bool sig);
+    void set_pp_parameters(Site* s1, Site* s2);
+    void set_pl_parameters(Site* s1, Site* s2);
+    void set_ll_parameters(Site* s1, Site* s2);
+    void print_params() const;
 
-        y[0]=s1->y();       // yc1
-        y[1]=s2->b()*alfa3; // alfa2*alfa3
-        y[2]=s2->b()*kk;      // -alfa2 = -b2
-        y[3]=s2->a();       // alfa1 = a2
-        y[4]=0;             // alfa4 = r1
-        y[5]=+1;            // lambda1 (allways positive offset from PointSite?)
-        y[6]=alfa3;         // alfa3
-        y[7]=kk;            // -1 = k2 side of line??
-    }
-    // line(s1)-line(s2) edge
-    void set_ll_parameters(Site* s1, Site* s2) {  // Held thesis p96
-        assert( s1->isLine() && s2->isLine() );
-        std::cout << " set_ll \n";
-        type = LINELINE;
-        //sign = true; //sign does not matter because there is no sqrt()?        
-        
-
-        double delta =  s1->a()*s2->b() - s1->b()*s2->a() ;
-        assert( delta != 0 );
-
-        //double kk=-1.0;
-        //if (delta>0)
-        //    kk=+1.0;
-
-        x[0]= ( (s1->b() * s2->c()) - (s2->b() * s1->c()) ) / delta;  // alfa1 = (b1*d2-b2*d1) / delta
-        x[1]=0;
-        //x[2]= kk*(s2->b()-s1->b())/delta; // -alfa3 = -( b2-b1 )
-        x[2]= -(s1->k()*s2->b()-s2->k()*s1->b())/delta; // -alfa3 = -( b2-b1 )
-
-        y[0]= ( (s2->a()*s1->c()) - (s1->a()*s2->c()) ) / delta;  // alfa2 = (a2*d1-a1*d2) / delta
-        y[1]= 0;
-        y[2]= -(s2->k()*s1->a()-s1->k()*s2->a())/delta;  // -alfa4 = -( a1-a2 )
-        //y[2]= kk*(s1->a()-s2->a())/delta;  // -alfa4 = -( a1-a2 )
-        
-        x[3]=0;  
-        x[4]=0;
-        x[5]=0;
-        x[6]=0;
-        x[7]=0;
-        y[3]=0; 
-        y[4]=0;
-        y[5]=0;
-        y[6]=0;
-        y[7]=0;
-    }
-    void print_params() const {
-        std::cout << "x-params: ";
-        for (int m=0;m<8;m++)
-            std::cout << x[m] << " ";
-        std::cout << "\n";
-        std::cout << "y-params: ";
-        for (int m=0;m<8;m++)
-            std::cout << y[m] << " ";
-        std::cout << "\n";
-    }
-    // arc: d=sqrt( sq(xc1-xc2) + sq(yc1-yc2) )
 };
 
 } // end namespace

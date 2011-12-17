107758679a8b00c88bb1b6b9f5ce356bc5ec4050
Anders Wallin <anders.e.e.wallin@gmail.com>
2011-12-17 20:48:20 +0200

    use boost::timer to time the program use boost::program_options to optionally set number of points

--- cpp_examples/tst2/main.cpp
+++ cpp_examples/tst2/main.cpp
@@ -1,28 +1,66 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 #include <openvoronoi/voronoidiagram.hpp>
 
 #include <boost/random.hpp>
+#include <boost/timer.hpp>
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 // random points
-int main() {
-    ovd::VoronoiDiagram* vd = new ovd::VoronoiDiagram(1,100);
+int main(int argc,char *argv[]) {
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("n", po::value<int>(), "set number of points")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    
+    unsigned int nmax = 100;
+
+    if (vm.count("n")) 
+        nmax = vm["n"].as<int>();
+    
+    std::cout << "Number of points: " << nmax << ".\n";
+    int bins = (int)sqrt(nmax);
+    ovd::VoronoiDiagram* vd = new ovd::VoronoiDiagram(1,10*bins);
     
     std::cout << vd->version() << "\n";
     
     boost::mt19937 rng(42);
     boost::uniform_01<boost::mt19937> rnd(rng);
     
-    unsigned int nmax = 100;
+    std::vector<ovd::Point> pts;
     for (unsigned int m=0;m<nmax;m++) {
-        double x = rnd();
-        double y = rnd();
+        double x = 0.8*rnd()-0.5;
+        double y = 0.8*rnd()-0.5;
         ovd::Point p(x,y);
+        pts.push_back(p);
+    }
+    boost::timer tmr;
+    BOOST_FOREACH(ovd::Point p, pts ) {
         vd->insert_point_site(p);
     }
-    
+    //tmr.stop();
+    double t = tmr.elapsed();
+    std::cout << t << " seconds \n";
+    double norm = nmax*log((double)nmax)/log(2.0);
+    std::cout << 1e6*t/norm << " us * n*log2(n)\n";
     std::cout << vd->print();
     return 0;
 }

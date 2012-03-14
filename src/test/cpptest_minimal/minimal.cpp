41fa9afcb8e8889f4526c3452133a7cb20f3adfd
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-03-15 00:33:23 +0200

    call build_type()

--- src/test/cpptest_minimal/minimal.cpp
+++ src/test/cpptest_minimal/minimal.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv) {
     // int bins: bins for face-grid search. roughly sqrt(n), where n is the number of sites is good according to Held.
      
     std::cout << ovd::version() << "\n"; // the git revision-string
+    std::cout << ovd::build_type() << "\n"; // the build tybe (Release, Debug, Profile, etc)
     ovd::Point p(0,0);
     vd->insert_point_site(p); // this returns an int-handle to the point-site, but we do not use it here.
     

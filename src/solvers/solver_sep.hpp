809a77699bab2e1355f5876e8922711c2bff5aa6
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-03-15 19:14:22 +0200

    comment out code that coverage-testing showed was never used

--- src/solvers/solver_sep.hpp
+++ src/solvers/solver_sep.hpp
@@ -81,21 +81,25 @@ int solve( Site* s1, double k1,
     assert( s1->isLine() && s2->isPoint() );
     // separator direction
     Point sv(0,0);
+    /* code-coverage testing shows this never happens...
     if (k2 == -1) { // was k2?? but k2 is allways +1??
         sv.x = s1->a(); //l1.a
         sv.y = s1->b(); //l1.b
-    } else {
+    } else */
+    {
         sv.x = -s1->a();
         sv.y = -s1->b();
     }
     if (debug) std::cout << " SEPSolver sv= "<< sv << "\n";
     double tsln(0);
+    /* code-coverage testing shows this never happens...
     if ( s3->isPoint() ) {
         double dx = s2->x() - s3->x();
         double dy = s2->y() - s3->y();
         tsln = -(dx*dx+dy*dy) / (2*( dx*sv.x+dy*sv.y  )); // check for divide-by-zero?
 
-    } else if (s3->isLine()) {
+    } else */
+    if (s3->isLine()) {
         tsln = -(s3->a()*s2->x()+s3->b()*s2->y()+s3->c()) / ( sv.x*s3->a() + sv.y*s3->b() + k3  );
     } else {
         assert(0);

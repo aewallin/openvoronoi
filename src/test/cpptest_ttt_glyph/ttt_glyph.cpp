db850a00a1416f23ad5cad3e80e2dad6444936a0
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-03-19 18:29:57 +0200

    add ttt_glyph example code as a test also.

---
+++ src/test/cpptest_ttt_glyph/ttt_glyph.cpp
@@ -0,0 +1,142 @@
+// OpenVoronoi example. Build VD for a single glyph from true-type-tracer.
+// true-type-tracer is from https://github.com/aewallin/truetype-tracer
+
+#include <string>
+#include <iostream>
+
+#include <boost/foreach.hpp>
+#include <boost/timer.hpp>
+#include <boost/program_options.hpp>
+
+#include "voronoidiagram.hpp"
+#include "checker.hpp"
+#include "version.hpp"
+#include "common/point.hpp"
+#include "utility/vd2svg.hpp"
+
+#include <ttt/ttt.hpp>
+#include <ttt/segment_writer.hpp>
+
+#define TTFONT "/usr/share/fonts/truetype/freefont/FreeSerif.ttf"
+
+Loops get_ttt_loops(int char_index=0) {
+    SEG_Writer my_writer; // this Writer writes G-code
+    my_writer.set_scale(  2.5095362377e-4 );
+    
+    std::string all_glyphs = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char c = all_glyphs.at(char_index);
+    std::string glyph(&c,1); //all_glyphs.at(char_index);
+    std::cout << " ttt glyph is " << glyph << "\n";
+    
+    Ttt t( &my_writer, glyph, false , TTFONT ); // ( Writer*, text, unicode?, path-to-font )
+    
+    Loops all_loops = my_writer.get_loops();
+    
+    extents ext = my_writer.get_extents();
+    std::cout << " x-range : " << ext.minx << " - " << ext.maxx << "\n";
+    std::cout << " y-range : " << ext.miny << " - " << ext.maxy << "\n";  
+    
+    Loops mod_loops;
+    // ttt returns loops with duplicate start/endpoints
+    // to avoid duplicates, remove the first point from each loop
+    BOOST_FOREACH(Loop l, all_loops) {
+        l.erase(l.begin()); // remove first element of loop
+        mod_loops.push_back(l);
+    }
+
+    return mod_loops;
+}
+
+namespace po = boost::program_options;
+
+// ttt example program
+int main(int argc,char *argv[]) {
+    // Declare the supported options.
+    po::options_description desc("This program calculates the voronoi diagram for a glyph from truetype-tracer.\nAllowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("c", po::value<int>(), "set character, an int in [0,51] ")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    
+    int char_index = 0;
+
+    if (vm.count("c")) {
+        int c = vm["c"].as<int>();
+        if ( 0<=c && c<= 51 )
+            char_index = c;
+        else {
+            std::cout << "--c option out of range!\n";
+            return 1;
+        }
+    }
+    Loops all_loops = get_ttt_loops(char_index);
+    
+    // print out the points
+    int nloop =0;
+    BOOST_FOREACH(Loop l, all_loops) {
+        std::cout << "Loop " << nloop << " has " << l.size() << " points:\n";
+        int npoint=0;
+        BOOST_FOREACH(Point pt, l) {
+            std::cout << " Point " << npoint << " x= " << pt.first << " y= " << pt.second << "\n";
+            npoint++;
+        }
+        std::cout << "End Loop " << nloop << "\n";
+        nloop++;
+    } 
+    
+    ovd::VoronoiDiagram* vd = new ovd::VoronoiDiagram(1,50);
+    std::cout << "OpenVoronoi version: " << ovd::version() << "\n";
+    
+    // store the verted IDs here, for later inserting line-segments
+    std::vector< std::vector<int> > loops_id;
+    typedef std::pair<double,double> ttt_pt;
+    boost::timer tmr;
+    int n_points=0;
+    BOOST_FOREACH(Loop loop, all_loops) {
+        std::vector<int> loop_id;
+        BOOST_FOREACH(ttt_pt pt, loop) {
+            loop_id.push_back( vd->insert_point_site( ovd::Point(pt.first,pt.second)) ); 
+            n_points++;
+        }
+        loops_id.push_back(loop_id);
+    }
+    std::cout << n_points << " PointSite:s inserted in  " << tmr.elapsed() << " seconds\n" << std::flush;
+    
+    // now we insert LineSite:s
+    tmr.restart();
+    int n_linesites=0;
+    BOOST_FOREACH( std::vector<int> loop_id, loops_id) {
+        for( unsigned int n=0; n<loop_id.size();n++) {
+            int next = n+1;
+            if (n==loop_id.size()-1)
+                next = 0;
+            vd->insert_line_site( loop_id[n], loop_id[next]); 
+            n_linesites++;
+        }
+    }
+    std::cout << n_linesites << " LineSites:s inserted in  " << tmr.elapsed() << " seconds\n" << std::flush;
+    
+    // write vd to svg file
+    std::string filename = "ttt_glyph.svg";
+    vd2svg(filename, vd);
+    std::cout << "Output written to SVG file: " << filename << "\n"; //tmr.elapsed() << " seconds\n" << std::flush;
+    
+    // sanity-check
+    ovd::VoronoiDiagramChecker check(vd->get_graph_reference());
+    if (!check.is_valid())
+        return -1;
+    else
+        std::cout << "ovd::VoronoiDiagramChecker OK!\n";
+    
+    delete vd;
+    return 0;
+}

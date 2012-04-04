ea52fa3229c987b81f9abb1d507cd255786b54a8
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-04-04 17:53:19 +0300

    visualize simple predicates

--- python_examples/arc_predicates.py
+++ python_examples/arc_predicates.py
@@ -5,7 +5,7 @@ import ovdvtk
 import vtk
 #import datetime
 import math
-#import random
+import random
 
 """
 This script does not use OpenVoronoi, it is used merely for drawing and
@@ -31,7 +31,10 @@ def drawCircle(myscreen, c, circleColor):
 
 def drawCircle(myscreen, c, r, circleColor):
     myscreen.addActor( ovdvtk.Circle( center=(c.x,c.y, 0), radius=r, color=circleColor ) )
-
+    
+def drawLine(myscreen, p1,p2, lineColor=ovdvtk.yellow):
+    myscreen.addActor( ovdvtk.Line( p1=( p1.x,p1.y,0), p2=(p2.x,p2.y,0), color=lineColor ) )
+    
 """
 def drawArc(myscreen, p1, p2, c, cw, arcColor):
     r = (p1-c).norm()
@@ -41,6 +44,7 @@ def drawArc(myscreen, p1, p2, c, cw, arcColor):
     
 # draw line  a x + b y + c = 0
 # draws lines roughly in a 100x100 box (?)
+"""
 def drawLine(myscreen, l, lineColor):
     #  x = -by-c / a
     #if l.a != 0:
@@ -56,6 +60,7 @@ def drawLine(myscreen, l, lineColor):
         x=-100
         p2 = ovd.Point( x, float(-l.a*x-l.c)/l.b )
         myscreen.addActor( ovdvtk.Line( p1=( p1.x,p1.y,0), p2=(p2.x,p2.y,0), color=lineColor ) )
+"""
 
 # CIRCLE definition
 # circle offset is  (x(t) - xc)^2 + (y(t)-yc)^2 = (r+k*t)^2
@@ -81,7 +86,13 @@ class Line:
 
 
 
+def arc_in_region(p1,p2,c,cw,p):
+    if cw:
+        return p.is_right(c,p1) and (not p.is_right(c,p2))
+    else:
+        return (not p.is_right(c,p1)) and p.is_right(c,p2)
 
+    
 def drawArcPredicate():
     myscreen = ovdvtk.VTKScreen()
     myscreen.camera.SetPosition(0.01, 0,  100 ) 
@@ -93,48 +104,112 @@ def drawArcPredicate():
     
     alfa1 = (float(23)/float(360) )* 2*math.pi
     alfa2 = (float(123)/float(360) )* 2*math.pi
+    
+    alfa2, alfa1 = alfa1, alfa2 # swap
+    
     alfa1_dir = ovd.Point( math.cos(alfa1), math.sin(alfa1) )
     alfa2_dir = ovd.Point( math.cos(alfa2), math.sin(alfa2) )
     p1 = c1 + r1*alfa1_dir
     p2 = c1 + r1*alfa2_dir
+    cw = True
     
     drawVertex(myscreen, c1, ovdvtk.orange, rad=1)
+    fa1 = ovdvtk.FollowerText(text="c",color=ovdvtk.orange,center=(c1.x+1,c1.y,0),scale=1)
+    myscreen.addActor(fa1)
+    
     drawVertex(myscreen, p1, ovdvtk.green, rad=1)
+    fa2 = ovdvtk.FollowerText(text="p1",color=ovdvtk.green,center=(p1.x+1,p1.y,0),scale=1)
+    myscreen.addActor(fa2)
+    
     drawVertex(myscreen, p2, ovdvtk.red, rad=1)    
+    fa3 = ovdvtk.FollowerText(text="p2",color=ovdvtk.red,center=(p2.x+1,p2.y,0),scale=1)
+    myscreen.addActor(fa3)
     
     #drawArc(myscreen, p1, p2, c1, True, ovdvtk.yellow)
-    ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, True, ovdvtk.yellow, da=0.1)
-    ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, False, ovdvtk.orange, da=0.1)
-    #c2 = ovd.Point(6,13)
+    #ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, True, ovdvtk.yellow, da=0.1)
+    ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, cw, ovdvtk.orange, da=0.1)
     
-    #r2=23
-    # external ma-pocket fails with this input:
-    """
-    c1 = ovd.Point(0, -25)
-    r1 = 5 #16.7033
-    c2 = ovd.Point(0, 0.2167)
-    r2 = 15.9299
-    r2 = 2 #16 # 5
-    drawCircle(myscreen, c1, r1, ovdvtk.red)
-    drawCircle(myscreen, c2, r2, ovdvtk.green)
-    # when machining c2 the maximum cut-width is 
-    # w_max = | c2 - c1 | + r2 - r1
-    #print "dr = ",dr
-    print " cut-width = ", ((c2-c1).norm()+r2-r1)
-    #[bd1,bd2] = bitanget_direction(c1,c2,r1,r2)
-    # from C, go a distance r along the normal to the line.
-    #p1 = c1 + r1*bd1
-    #p2 = c1 + r1*bd2
-    drawCircle(myscreen,p1,0.5,ovdvtk.pink)
-    drawCircle(myscreen,p2,0.5,ovdvtk.red)
+    Nmax = 5000
+    for n in range(Nmax):
+        p = 100*ovd.Point( random.random()-0.5, random.random()-0.5 )
+        if arc_in_region(p1,p2,c1,cw,p):
+            drawVertex(myscreen, p, ovdvtk.lgreen, rad=0.1)
+        else:
+            drawVertex(myscreen, p, ovdvtk.pink, rad=0.1)
 
-    p3 = c2 + r2*bd1
-    p4 = c2 + r2*bd2
-    drawCircle(myscreen,p3,0.5,ovdvtk.lgreen)
-    drawCircle(myscreen,p4,0.5,ovdvtk.green)
-    """
     myscreen.render()
     myscreen.iren.Start()
+
+def closer_endpoint(p1,p2,p):
+    if (p1-p).norm() < (p2-p).norm():
+        return p1
+    else:
+        return p2
+        
+def projection_point(p1,p2,c1,cw,p):
+    if p==c1:
+        return p1
+    else:
+        n = (p-c1)
+        n.normalize()
+        return c1 + (p1-c1).norm()*n
+        
+def apex_point(p1,p2,c1,cw,p):
+    if arc_in_region(p1,p2,c1,cw,p):
+        return projection_point(p1,p2,c1,cw,p)
+    else:
+        return closer_endpoint(p1,p2,p)
+            
+def drawArcPredicate2():
+    myscreen = ovdvtk.VTKScreen()
+    myscreen.camera.SetPosition(0.01, 0,  100 ) 
+    myscreen.camera.SetFocalPoint(0, 0, 0)
+    myscreen.camera.SetClippingRange(-100,3000)
     
-if __name__ == "__main__":  
-    drawArcPredicate()
+    c1 = ovd.Point(0,0)
+    r1=20
+    
+    alfa1 = (float(23)/float(360) )* 2*math.pi
+    alfa2 = (float(123)/float(360) )* 2*math.pi
+    
+    #alfa2, alfa1 = alfa1, alfa2 # swap
+    
+    alfa1_dir = ovd.Point( math.cos(alfa1), math.sin(alfa1) )
+    alfa2_dir = ovd.Point( math.cos(alfa2), math.sin(alfa2) )
+    p1 = c1 + r1*alfa1_dir
+    p2 = c1 + r1*alfa2_dir
+    cw = False
+    
+    drawVertex(myscreen, c1, ovdvtk.orange, rad=1)
+    fa1 = ovdvtk.FollowerText(text="c",color=ovdvtk.orange,center=(c1.x+1,c1.y,0),scale=1)
+    myscreen.addActor(fa1)
+    
+    drawVertex(myscreen, p1, ovdvtk.green, rad=1)
+    fa2 = ovdvtk.FollowerText(text="p1",color=ovdvtk.green,center=(p1.x+1,p1.y,0),scale=1)
+    myscreen.addActor(fa2)
+    
+    drawVertex(myscreen, p2, ovdvtk.red, rad=1)    
+    fa3 = ovdvtk.FollowerText(text="p2",color=ovdvtk.red,center=(p2.x+1,p2.y,0),scale=1)
+    myscreen.addActor(fa3)
+    
+    #drawArc(myscreen, p1, p2, c1, True, ovdvtk.yellow)
+    #ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, True, ovdvtk.yellow, da=0.1)
+    ovdvtk.drawArc(myscreen, p1, p2, (p1-c1).norm(), c1, cw, ovdvtk.orange, da=0.1)
+    
+    Nmax = 5000
+    for n in range(Nmax):
+        p = 100*ovd.Point( random.random()-0.5, random.random()-0.5 )
+        
+        apex = apex_point(p1,p2,c1,cw,p)
+        linecolor= ovdvtk.pink
+        if arc_in_region(p1,p2,c1,cw,p):
+            linecolor = ovdvtk.lgreen
+        
+        drawLine(myscreen,p,apex,linecolor)
+
+    myscreen.render()
+    myscreen.iren.Start()
+
+if __name__ == "__main__":
+    #drawArcPredicate()
+    drawArcPredicate2()

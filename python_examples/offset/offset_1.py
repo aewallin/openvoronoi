003e6ad64702a0713854541bbce2b42654fd2cb6
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-07-27 12:43:36 +0300

    remove common code

--- python_examples/offset/offset_1.py
+++ python_examples/offset/offset_1.py
@@ -4,83 +4,7 @@ import time
 import vtk
 import math
 
-# draw a line from pt1 to pt2 with the given color
-def drawLine(myscreen, pt1, pt2, lineColor):
-    myscreen.addActor( ovdvtk.Line(p1=(pt1.x,pt1.y,0),p2=(pt2.x,pt2.y,0),color=lineColor) ) 
-
-# rotate a point in 2D by cos/sin. from emc2 gcodemodule.cc
-def rotate(x, y,  c,  s):
-    tx = x * c - y * s;
-    y = x * s + y * c;
-    x = tx;
-    return [x,y]
-
-# draw an arc using many line-segments
-# start at pt1, and at pt2, center at cen
-# radius r, cw is a bool cw/ccw flag
-def drawArc(myscreen, pt1, pt2, r, cen,cw,arcColor):
-    start = pt1-cen
-    end = pt2-cen
-    theta1 = math.atan2(start.x,start.y)
-    theta2 = math.atan2(end.x,end.y)
-    alfa=[] # the list of angles
-    da=0.1
-    CIRCLE_FUZZ = 1e-9
-    # idea from emc2 / cutsim g-code interp G2/G3
-    if (cw == False ): 
-        while ( (theta2 - theta1) > -CIRCLE_FUZZ): 
-            theta2 -= 2*math.pi
-    else:
-        while( (theta2 - theta1) < CIRCLE_FUZZ): 
-            theta2 += 2*math.pi
-    
-    dtheta = theta2-theta1
-    arclength = r*dtheta
-    dlength = min(0.01, arclength/10)
-    steps = int( float(arclength) / float(dlength))
-    rsteps = float(1)/float(steps)
-    dc = math.cos(-dtheta*rsteps) # delta-cos  
-    ds = math.sin(-dtheta*rsteps) # delta-sin
-    
-    previous = pt1
-    tr = [start.x, start.y]
-    for i in range(steps):
-        tr = rotate(tr[0], tr[1], dc, ds) #; // rotate center-start vector by a small amount
-        x = cen.x + tr[0] 
-        y = cen.y + tr[1] 
-        current = ovd.Point(x,y)
-        myscreen.addActor( ovdvtk.Line(p1=(previous.x,previous.y,0),p2=(current.x,current.y,0),color=arcColor) )
-        previous = current 
-
-# draw many offsets
-def drawOffsets(myscreen, ofs):
-    # draw loops
-    nloop = 0
-    lineColor = ovdvtk.green
-    arcColor = ovdvtk.grass
-    for lop in ofs:
-        n = 0
-        N = len(lop)
-        first_point=[]
-        previous=[]
-        for p in lop:
-            # p[0] is the Point
-            # p[1] is -1 for lines, and r for arcs
-            if n==0: # don't draw anything on the first iteration
-                previous=p[0]
-            else:
-                cw=p[3]
-                cen=p[2]
-                r=p[1]
-                p=p[0]
-                if r==-1: # this offset element is a line
-                    drawLine(myscreen, previous, p, lineColor)
-                else: # this offset element is an arc
-                    drawArc(myscreen, previous, p, r,cen,cw, arcColor)
-                previous=p
-            n=n+1
-        print "rendered loop ",nloop, " with ", len(lop), " points"
-        nloop = nloop+1
+import offset2vtk
 
 if __name__ == "__main__":  
     #w=2500 # screen resolution for big screens
@@ -166,7 +90,7 @@ if __name__ == "__main__":
     of = ovd.Offset( vd.getGraph() ) # pass the created graph to the Offset class
     of.str() # text output, for debug
     ofs = of.offset(0.123) # generate offsets at the given distance.
-    drawOffsets(myscreen, ofs) # draw the generated offsets
+    offset2vtk.drawOffsets(myscreen, ofs) # draw the generated offsets
 
     vod.setVDText2(times)
     vod.setAll()

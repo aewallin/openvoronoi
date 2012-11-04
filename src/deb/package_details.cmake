034ce314d29b0c83ddca50347435fb207b9eb0cc
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-11-04 19:17:32 +0200

    build package for quantal

--- src/deb/package_details.cmake
+++ src/deb/package_details.cmake
@@ -14,12 +14,13 @@ set(CPACK_DEBIAN_PACKAGE_SECTION "science" CACHE STRING "name3")
 set(DEBSRC_BUILD_DEPENDS debhelper python git cmake libboost-dev "libqd0 | libqd2c2a" libqd-dev libboost-python-dev libgomp1 CACHE STRINGS "name")
 
 # we need to explicitly list the libboost-python versions here. why??
+# quantal has 1.49.0
 # precise has 1.48.0
 # oneiric has 1.46.1
 # natty/maverick has 1.42.0
 # lucid has 1.40.0
 set(DEBSRC_PACKAGE_DEPENDS python git cmake 
-                "libboost-python1.48.0 | libboost-python1.46.1 | libboost-python1.42.0 | libboost-python1.40.0"
+                "libboost-python1.49.0 | libboost-python1.48.0 | libboost-python1.46.1 | libboost-python1.42.0 | libboost-python1.40.0"
                 "libqd0 | libqd2c2a" CACHE STRING "name")
 
 # however CPack wants dependencies as a single comma separated string!
@@ -46,7 +47,7 @@ set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${DEB_ARCHITECTURE} CACHE STRING "name6")
 set(CPACK_DEBIAN_PACKAGE_PRIORITY optional CACHE STRING "name7")
 SET(CPACK_PACKAGE_VERSION ${MY_VERSION} CACHE STRING "name8")
 set(CPACK_DEBIAN_DISTRIBUTION_NAME ubuntu CACHE STRING "name9")
-set(CPACK_DEBIAN_DISTRIBUTION_RELEASES lucid maverick natty oneiric precise CACHE STRING "name10") 
+set(CPACK_DEBIAN_DISTRIBUTION_RELEASES lucid maverick natty oneiric precise quantal CACHE STRING "name10") 
 # message(STATUS " CMAKE_SOURCE_DIR is = " ${CMAKE_SOURCE_DIR})
 if(${SRC_DIR} MATCHES "")
     set(CPACK_PACKAGE_DESCRIPTION_FILE ${SRC_DIR}/deb/debian_package_description.txt CACHE STRING "package description file")

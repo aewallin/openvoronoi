b9bcdfa5159f19e23cae7a3ad06b173318fda093
pedersen <jtp@cs.au.dk>
2017-03-05 23:00:23 +0100

    move SvgReader to separate implementation file

--- python_examples/ma-offset/svg_offset.py
+++ python_examples/ma-offset/svg_offset.py
@@ -1,146 +1,18 @@
 import openvoronoi as ovd
 import ovdvtk
 
-from svgpathtools import svg2paths2
-
-
 import time
 import vtk
 import math
 
 import offset2vtk
 
-def sub(p1, p2):
-    return (p1[0] - p2[0], p1[0] - p2[0])
-def add(p1, p2):
-    return (p1[0] + p2[0], p1[0] + p2[0])
-def mul(p, f):
-    return (p[0] *f, p[1]*f)
-def mag(p):
-    return math.sqrt(p[0]*p[0]+p[1]*p[1])
-def normalize(v):
-    return v.mul(1.0/mag(v))
-def tup(p):
-    return (p.real, p.imag)
-def dist(p1, p2):
-    return mag(sub(p1, p2))
-
-class SvgReader:
-
-    def __init__(self, filename):
-        self.paths, self.attributes, svg_attributes = svg2paths2(filename)
-        self.polys = []
-        self.miny = self.minx = float("+inf")
-        self.maxy = self.maxx = float("-inf")
-        #print("svg_attributes[idx]:{}".format(svg_attributes))
-
-    def path_to_poly(self, path):
-        """Return a list of points that forms a closed polygon
-
-        TODO:
-        If the path is not a line the steps parameter will divide the
-        segment into \p steps steps
-        """
-        pts = []
-        for segment in path:
-            pts.extend(self.adaptiveAdd(segment, 0, 1))
-        if not path.isclosed():
-            print("Path is not closed!:")
-            p = path[0].point(0)
-            self.addPoint(pts, p.real, p.imag)
-        return pts
-
-    def adaptiveAdd(self, segment, t0, t1):
-        """Adaptively sample segment
+import sys
+import os
+sys.path.append( os.path.join(os.path.dirname(__file__), "../"))
+from SvgReader import SvgReader
 
 
-        """
-        p = tup(segment.point(t0))
-        p2 = tup(segment.point(t1))
-        v = sub(p2, p)
-        d = mag(v)
-        if d ==0:
-            print("no dist")
-            return [p]
-        t1_2 = (t0+t1)/2.0
-        half = add(p,mul(mul(v, 1.0/d), d*.5))
-        half_p = tup(segment.point(t1_2))
-        diff = dist(half, half_p)
-        error = diff/d
-        if d < .01  or error < .01:
-            self.registerPoint(*p)
-            return [p]
-        else:
-            #print("Error {}".format(int(100*error)))
-            self.registerPoint(*half_p)
-            first = self.adaptiveAdd(segment, t0, t1_2)
-            res = []
-            if (first):
-                res = first
-            second = self.adaptiveAdd(segment, t1_2, t1)
-            if second:
-                res.extend(second)
-            return res
-
-
-    def registerPoint(self, x,y):
-        self.miny = min(y, self.miny)
-        self.minx = min(x, self.minx)
-        self.maxy = max(y, self.maxy)
-        self.maxx = max(x, self.maxx)
-
-    def addPoint(self, ls, x, y):
-        registerPoint(x,y)
-        ls.append((x,y))
-
-    def parse(self):
-        """Parse all paths to linear polygons
-
-        Determine AABB, center and store transform
-        """
-        for path in self.paths:
-            for continuous_subpath in path.continuous_subpaths():
-                poly = self.path_to_poly(continuous_subpath)
-                if poly:
-                    self.polys.append(poly)
-
-        center = (self.maxx + self.minx/2, (self.maxy + self.miny)/2)
-        w = self.maxx - self.minx
-        h = self.maxy - self.miny
-        self.radius = math.sqrt(w*w + h*h)
-        print("miny:{}".format(self.miny))
-        print("minx:{}".format(self.minx))
-        print("maxy:{}".format(self.maxy))
-        print("maxx:{}".format(self.maxx))
-        print("center:{}".format(center))
-        print("radius:{}".format(self.radius))
-        self.offsetx = center[0]
-        self.offsety = center[1]
-
-
-
-        adjusted = []
-        for poly in self.polys:
-            ls = []
-            for p in poly:
-                ls.append(self.offsetPoint(p))
-   #             ls.reverse()
-            adjusted.append(ls)
-        self.polys = adjusted
-    def offsetPoint(self, p):
-        return (p[0] - self.offsetx,  p[1] - self.offsety)
-
-    def bbox(self):
-        return [
-            self.offsetPoint( (self.minx-2, self.maxy +2)),
-            self.offsetPoint( (self.maxx+2, self.maxy +2)),
-            self.offsetPoint( (self.maxx+2, self.miny-2)),
-            self.offsetPoint( (self.minx-2, self.miny-2)),
-            ]
-
-
-    def __getitem__(self, index):
-        return self.polys[index]
 
 def insert_polygon_points(vd, poly):
     pts=[]
@@ -221,7 +93,8 @@ if __name__ == "__main__":
 
     svgr = SvgReader(svg)
     svgr.parse()
-
+    svgr.centerPolys()
+    print(svgr.stats())
 
     myscreen = ovdvtk.VTKScreen(width=w, height=h) 
     ovdvtk.drawOCLtext(myscreen, rev_text=ovd.version() )   

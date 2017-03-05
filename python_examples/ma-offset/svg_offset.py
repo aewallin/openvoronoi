31c0d53648b0853d8a710af0a22a984f9cd2f8e1
pedersen <jtp@cs.au.dk>
2017-03-05 21:04:51 +0100

    adaptivly sample svg path

--- python_examples/ma-offset/svg_offset.py
+++ python_examples/ma-offset/svg_offset.py
@@ -10,6 +10,21 @@ import math
 
 import offset2vtk
 
+def sub(p1, p2):
+    return (p1[0] - p2[0], p1[0] - p2[0])
+def add(p1, p2):
+    return (p1[0] + p2[0], p1[0] + p2[0])
+def mul(p, f):
+    return (p[0] *f, p[1]*f)
+def mag(p):
+    return math.sqrt(p[0]*p[0]+p[1]*p[1])
+def normalize(v):
+    return v.mul(1.0/mag(v))
+def tup(p):
+    return (p.real, p.imag)
+def dist(p1, p2):
+    return mag(sub(p1, p2))
+
 class SvgReader:
 
     def __init__(self, filename):
@@ -28,19 +43,54 @@ class SvgReader:
         """
         pts = []
         for segment in path:
-            p = segment.point(0)
-            self.addPoint(pts, p.real, p.imag)
+            pts.extend(self.adaptiveAdd(segment, 0, 1))
         if not path.isclosed():
             print("Path is not closed!:")
             p = path[0].point(0)
             self.addPoint(pts, p.real, p.imag)
         return pts
 
-    def addPoint(self, ls, x, y):
+    def adaptiveAdd(self, segment, t0, t1):
+        """Adaptively sample segment
+
+
+        """
+        p = tup(segment.point(t0))
+        p2 = tup(segment.point(t1))
+        v = sub(p2, p)
+        d = mag(v)
+        if d ==0:
+            print("no dist")
+            return [p]
+        t1_2 = (t0+t1)/2.0
+        half = add(p,mul(mul(v, 1.0/d), d*.5))
+        half_p = tup(segment.point(t1_2))
+        diff = dist(half, half_p)
+        error = diff/d
+        if d < .01  or error < .01:
+            self.registerPoint(*p)
+            return [p]
+        else:
+            #print("Error {}".format(int(100*error)))
+            self.registerPoint(*half_p)
+            first = self.adaptiveAdd(segment, t0, t1_2)
+            res = []
+            if (first):
+                res = first
+            second = self.adaptiveAdd(segment, t1_2, t1)
+            if second:
+                res.extend(second)
+            return res
+
+
+    def registerPoint(self, x,y):
         self.miny = min(y, self.miny)
         self.minx = min(x, self.minx)
         self.maxy = max(y, self.maxy)
         self.maxx = max(x, self.maxx)
+
+    def addPoint(self, ls, x, y):
+        registerPoint(x,y)
         ls.append((x,y))
 
     def parse(self):
@@ -55,22 +105,54 @@ class SvgReader:
                     self.polys.append(poly)
 
         center = (self.maxx + self.minx/2, (self.maxy + self.miny)/2)
-        self.radius = math.sqrt((self.minx-center[0])*(self.minx-center[0]) + (self.miny-center[1])*(self.miny-center[1]))
+        w = self.maxx - self.minx
+        h = self.maxy - self.miny
+        self.radius = math.sqrt(w*w + h*h)
         print("miny:{}".format(self.miny))
         print("minx:{}".format(self.minx))
         print("maxy:{}".format(self.maxy))
         print("maxx:{}".format(self.maxx))
         print("center:{}".format(center))
         print("radius:{}".format(self.radius))
+        self.offsetx = center[0]
+        self.offsety = center[1]
+
 
 
         adjusted = []
         for poly in self.polys:
             ls = []
             for p in poly:
-                adjustedPoint = (p[0] - center[0],  p[1] - center[1])
+                ls.append(self.offsetPoint(p))
+   #             ls.reverse()
+            adjusted.append(ls)
+        self.polys = adjusted
+    def offsetPoint(self, p):
+        return (p[0] - self.offsetx,  p[1] - self.offsety)
+
+    def bbox(self):
+        return [
+            self.offsetPoint( (self.minx-2, self.maxy +2)),
+            self.offsetPoint( (self.maxx+2, self.maxy +2)),
+            self.offsetPoint( (self.maxx+2, self.miny-2)),
+            self.offsetPoint( (self.minx-2, self.miny-2)),
+            ]
+
+
+    def __getitem__(self, index):
+        return self.polys[index]
+
+def insert_polygon_points(vd, poly):
+    pts=[]
+    for p in poly:
+        offsetx = center[0]
+        offsety = center[1]
+        for poly in self.polys:
+            ls = []
+            for p in poly:
+                adjustedPoint = (p[0] - offsetx,  p[1] - offsety)
                 ls.append(adjustedPoint)
-#            ls.reverse()
+   #             ls.reverse()
             adjusted.append(ls)
         self.polys = adjusted
 
@@ -80,7 +162,7 @@ class SvgReader:
 def insert_polygon_points(vd, poly):
     pts=[]
     for p in poly:
-        print("Inserting:{}".format(p))
+        #print("Inserting:{}".format(p))
         pts.append( ovd.Point( *p ))
     id_list = []
     print "inserting ",len(pts)," point-sites:"
@@ -120,47 +202,10 @@ def insert_many_polygons(vd, svgr):
 
     return [pt_time, seg_time]
 
-def translate(segs,x,y):
-    out = []
-    for seg in segs:
-        seg2 = []
-        for p in seg:
-            p2 = []
-            p2.append(p[0] + x)
-            p2.append(p[1] + y)
-            seg2.append(p2)
-        out.append(seg2)
-    return out
-
-def ttt_segments(text,scale):
-    wr = ttt.SEG_Writer()
-    wr.arc = False
-    wr.conic = False
-    wr.cubic = False
-
-    wr.conic_biarc_subdivision = 10 # this has no effect?
-    wr.conic_line_subdivision = 50 # this increases nr of points 
-    wr.cubic_biarc_subdivision = 10 # no effect?
-    wr.cubic_line_subdivision = 10 # no effect?
-    wr.setFont(3)
-    
-    wr.scale = float(1)/float(scale)
-    ttt.ttt(text,wr) 
-    segs = wr.get_segments()
-    return segs
-
-
-def modify_segments(segs):
-    segs_mod =[]
-    for seg in segs:
-        first = seg[0]
-        last = seg[ len(seg)-1 ]
-        assert( first[0]==last[0] and first[1]==last[1] )
-        seg.pop()
-        seg.reverse() # to get interior or exterior offsets
-        segs_mod.append(seg)
-        #drawSegment(myscreen, seg)
-    return segs_mod
+def insert_bb(vd, svgr):
+    polygon_ids =[]
+    poly_id = insert_polygon_points(vd, svgr.bbox())
+    insert_polygon_segments(vd,poly_id)
 
 if __name__ == "__main__":  
     #print ocl.revision()
@@ -180,19 +225,18 @@ if __name__ == "__main__":
 
     myscreen = ovdvtk.VTKScreen(width=w, height=h) 
     ovdvtk.drawOCLtext(myscreen, rev_text=ovd.version() )   
-    
+
     scale=1
     myscreen.render()
 
-    far = svgr.radius * 1.1
+    far = svgr.radius * 1.5
     camPos = far
     zmult = 0.5
     # camPos/float(1000)
     myscreen.camera.SetPosition(0, -camPos/float(1000), zmult*camPos) 
     myscreen.camera.SetClippingRange(-(zmult+1)*camPos,(zmult+1)*camPos)
     myscreen.camera.SetFocalPoint(0.0, 0, 0)
-    
-    
+
     vd = ovd.VoronoiDiagram(far,120)
     print ovd.version()
     
@@ -207,29 +251,25 @@ if __name__ == "__main__":
     vod.drawGenerators=0
     vod.offsetEdges = 0
     vd.setEdgeOffset(0.05)
-
     times = insert_many_polygons(vd,svgr)
+
     print "all sites inserted. "
     print "VD check: ", vd.check()
-    
+
     pi = ovd.PolygonInterior(  True )
     vd.filter_graph(pi)
-    
+
     of = ovd.Offset( vd.getGraph() ) # pass the created graph to the Offset class
     step = svgr.radius *.01
     ofs_dist= step
     ofs = []
-    for n in range(50):
+    for n in range(15):
         ofsx = of.offset(ofs_dist) # generate offsets at the given distance.
         ofs.extend(ofsx)
         ofs_dist = ofs_dist + step
-        
     offset2vtk.drawOffsets2(myscreen, ofs) # draw the generated offsets
-    
-    
     #ma = ovd.MedialAxis(1)
     #vd.filter_graph(ma)
-    
     vod.setVDText2(times)
     vod.setAll()
     print "PYTHON All DONE."

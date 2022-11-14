bb3cb4c50c25b88ae9ecf6997b489b2d2568e465
Anders Wallin <anders.e.e.wallin@gmail.com>
2022-11-14 20:09:59 +0200

    2to3 for test

--- src/test/pytest_random_linesegments/random_linesegments.py
+++ src/test/pytest_random_linesegments/random_linesegments.py
@@ -9,8 +9,8 @@ import os
 
 def test(Nmax):
     vd = ovd.VoronoiDiagram(1,120)
-    print "random_linesegments.py:  ",Nmax," random segments."
-    print "in directory: ", os.getcwd()
+    print("random_linesegments.py:  ",Nmax," random segments.")
+    print("in directory: ", os.getcwd())
     sys.stdout.flush()
     f1 = "../../src/test/data/randomsegments_{0}.pickle.gz".format(Nmax)
     f2 = "../data/randomsegments_{0}.pickle.gz".format(Nmax)
@@ -20,7 +20,7 @@ def test(Nmax):
     elif os.path.exists( f2 ):
         filename = f2
     # (produced with lineseg_dataset_generator.py)
-    print "trying to open data file ",filename
+    print("trying to open data file ",filename)
     f = gzip.open(filename, 'rb')
     pstring = f.read()
     segs = pickle.loads( pstring )
@@ -32,28 +32,28 @@ def test(Nmax):
         seg_id.append( vd.addVertexSite( seg[0] ) )
         seg_id.append( vd.addVertexSite( seg[1] ) )
         id_list.append( seg_id )
-    print "all point-sites inserted."
+    print("all point-sites inserted.")
     assert( vd.check() )
     nsegs = Nmax
     for s in id_list:
         vd.addLineSite(s[0],s[1])
-    print "all line-sites inserted."
+    print("all line-sites inserted.")
     c = vd.check()
-    print " VD check: ", c
-    print sys.version
-    print ovd.version()
+    print(" VD check: ", c)
+    print(sys.version)
+    print(ovd.version())
     if c:
-        print "Test passed."
+        print("Test passed.")
         #exit(0)
     else:
-        print "Test failed."
+        print("Test failed.")
         #exit(-1)
-    print "end of test()"
+    print("end of test()")
     
 if __name__ == "__main__":  
     Nmax = 128 
     if len(sys.argv) == 2:
         Nmax  = int(sys.argv[1])
     test(Nmax)
-    print "end of script"
+    print("end of script")
 
